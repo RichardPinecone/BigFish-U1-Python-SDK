a15ba0fb01e930839548daa7663fd8363324323e
RichardPinecone <48935472+RichardPinecone@users.noreply.github.com>
2019-05-06 14:51:07 +0800

    Create timer.py

---
+++ examples/timer.py
@@ -0,0 +1,18 @@
+import pyb
+
+# create TIMER instance, only support 0~3
+a = pyb.TIMER(0)
+
+# define one timer callback
+def func(timer_channel)
+   print("timer interrupt\n")
+
+# set callback function
+a.callback(func)
+
+# set timer period to 500ms, and timer is enabled automatically
+a.period(500)
+
+# disable timer
+a.disable()
+
